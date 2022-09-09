0e47171a8c39f1c56073bf54ad19fa1f1759bea6
Pieris128 <elpichichi2048@gmail.com>
2022-09-08 21:10:48 -0300

    Finished Sorting Querys Practice

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,8 +2,41 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    // 1a) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = [
+      'page',
+      'sort',
+      'limit',
+      'fields',
+    ];
+    excludedFields.forEach(
+      (el) => delete queryObj[el]
+    );
 
+    //1b) Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort
+        .split(',')
+        .join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
