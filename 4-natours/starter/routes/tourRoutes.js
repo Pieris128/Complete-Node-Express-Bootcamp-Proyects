1e43736259e2922f23c654ccab78b6e2dc0a6548
Pieris128 <elpichichi2048@gmail.com>
2022-09-14 20:19:46 -0300

    MongoDB with Mongoose Section Finished

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -16,6 +16,10 @@ router
   .get(tourController.getTourStats);
 
 router
+  .route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
